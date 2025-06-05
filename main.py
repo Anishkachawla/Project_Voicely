bd94ba596941d5f0fbaaa8c854c8f18001fdd9b3
Anishkachawla <davanishka10i10@gmail.com>
2025-06-06 01:48:10 +0530

    Listen Command

--- main.py
+++ main.py
@@ -11,3 +11,23 @@ def speak(text):
 
 if __name__ == "__main__":
     speak("Intializing Voicely......")
+    while True:
+        # Listen for the wake word "Voicely"
+        # Obtain audio from microphone
+        r = sr.Recognizer()
+        
+        try:
+            with sr.Microphone() as source:
+                print("Listening......")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+                print("Recognizing.....")
+            word = r.recognize_google(audio)
+            if(word.lower() == "voicely"):
+                speak("Ya")
+                #Listen for command
+                with sr.Microphone() as source:
+                    print("Voicely active......")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+        except Exception as e:
+            print("Error; {0}".format(e))
